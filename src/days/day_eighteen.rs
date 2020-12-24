4b3e90228716ad1df9f9f3fa2e43cc6c5e4f0d2b
James Turner <james.turner@metaswitch.com>
2020-12-24 00:09:10 +0000

    Finish day eighteen

--- src/days/day_eighteen.rs
+++ src/days/day_eighteen.rs
@@ -1,10 +1,91 @@
 use crate::common::read_input;
 use anyhow::Result;
+use regex::Regex;
+
+lazy_static::lazy_static! {
+    pub static ref BRACKETED: regex::Regex =
+        Regex::new(r"(\([^\()]*\))").unwrap();
+}
+
+lazy_static::lazy_static! {
+    pub static ref UNBRACKETED: regex::Regex =
+        Regex::new(r"\((.+)\)").unwrap();
+}
+
+lazy_static::lazy_static! {
+    pub static ref NUMBER: regex::Regex =
+        Regex::new(r"\d+").unwrap();
+}
+
+lazy_static::lazy_static! {
+    pub static ref PLUS: regex::Regex =
+        Regex::new(r"(\d+) \+ (\d+)").unwrap();
+}
 
 pub fn day_eighteen() -> Result<()> {
-    let inputs: Vec<Vec<char>> = read_input("input/day_eighteen.txt")?
-        .map(|x| x.chars().collect())
-        .collect();
+    let result: u64 = read_input("input/day_eighteen.txt")?
+        .map(unbracket)
+        .map(|x| x.unwrap())
+        .sum();
+
+    println!("{:?}", result);
 
     Ok(())
+}
+
+// Process. Replace each bracketed segment until there are no segments left
+fn unbracket(mut input: String) -> Result<u64> {
+    while let Some(bmatch) = BRACKETED.find(&input.clone()) {
+        // Resolve the brackets
+        let resolved =
+            resolve_part_two(&UNBRACKETED.captures(bmatch.as_str()).unwrap()[1].to_string())?;
+        // Replace with the result of the brackets
+        input.replace_range(bmatch.range(), &format!("{:?}", resolved));
+    }
+
+    resolve_part_two(&input)
+}
+
+fn resolve_part_one(input: &str) -> Result<u64> {
+    let inner = input.split(" ");
+
+    let mut result = 0;
+    let mut operation = "+";
+
+    for thing in inner {
+        if NUMBER.is_match(thing) {
+            if operation == "+" {
+                result += thing.parse::<u64>()?
+            } else {
+                result *= thing.parse::<u64>()?
+            }
+        } else {
+            operation = thing;
+        }
+    }
+
+    Ok(result)
+}
+
+fn unplus(input: &str) -> Result<String> {
+    let mut input = input.to_string();
+
+    while let Some(plus_match) = PLUS.find(&input.clone()) {
+        // Resolve the plus
+        let plus_cap = &PLUS.captures(&input).unwrap();
+        let resolved = plus_cap[1].parse::<u64>()? + plus_cap[2].parse::<u64>()?;
+        // Replace with the result of the brackets
+        input.replace_range(plus_match.range(), &format!("{:?}", resolved));
+    }
+
+    Ok(input)
+}
+
+fn resolve_part_two(input: &str) -> Result<u64> {
+    let input = unplus(input)?;
+
+    Ok(input
+        .split(" ")
+        .map(|x| x.parse::<u64>().unwrap_or_else(|x| 1))
+        .fold(1, |acc, x| x * acc))
 }
