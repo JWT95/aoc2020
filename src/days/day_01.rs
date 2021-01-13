e5450015a08e4749d524ca27e31d642ca9df570c
James Turner <james.turner@metaswitch.com>
2021-01-13 16:40:55 +0000

    Use combinations

--- src/days/day_01.rs
+++ src/days/day_01.rs
@@ -1,42 +1,33 @@
 use crate::common::read_input;
 use anyhow::Result;
+use itertools::Itertools as _;
 
-fn _part_one(inputs: Vec<i64>) {
-    for x in &inputs {
-        for y in &inputs {
-            if x + y == 2020 {
-                println!("x: {}, y: {}, multiplier: {}", x, y, x * y);
-                return;
-            }
+fn _part_one(inputs: &[u32]) {
+    for pair in inputs.iter().combinations(2) {
+        let (x, y) = (pair[0], pair[1]);
+        if x + y == 2020 {
+            println!("x: {}, y: {}, multiplier: {}", x, y, x * y);
+            return;
         }
     }
 }
 
-fn part_two(inputs: Vec<i64>) {
-    for x in &inputs {
-        for y in &inputs {
-            for z in &inputs {
-                if x + y + z == 2020 {
-                    println!(
-                        "x: {}, y: {}, z: {},  multiplier: {}",
-                        x,
-                        y,
-                        x * y,
-                        x * y * z
-                    );
-                    return;
-                }
-            }
+fn part_two(inputs: &[u32]) {
+    for triple in inputs.iter().combinations(3) {
+        let (x, y, z) = (triple[0], triple[1], triple[2]);
+        if x + y + z == 2020 {
+            println!("x: {}, y: {}, z: {}, multiplier: {}", x, y, z, x * y * z);
+            return;
         }
     }
 }
 
 pub fn day_01() -> Result<()> {
-    let inputs: Vec<i64> = read_input("input/day_01.txt")?
-        .map(|x| x.parse::<i64>().unwrap())
+    let inputs: Vec<u32> = read_input("input/day_01.txt")?
+        .map(|x| x.parse::<u32>().unwrap())
         .collect();
 
-    part_two(inputs);
+    part_two(&inputs);
 
     Ok(())
 }
